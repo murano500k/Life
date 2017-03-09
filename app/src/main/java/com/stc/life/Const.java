e7116467ddfd4c07d01a5dac69153d49ec6a33ad
Artem <murano500k@gmail.com>
2017-03-09 10:53:09 +0200

    first steps

---
+++ app/src/main/java/com/stc/life/Const.java
@@ -0,0 +1,14 @@
+package com.stc.life;
+
+/**
+ * Created by artem on 3/9/17.
+ */
+
+public class Const {
+    private static final int MAP_WIDTH = 10;
+private static final int MAP_HEIGTH = 10;
+    private static final int SLEEP_TIME = 1000;
+
+
+
+}
